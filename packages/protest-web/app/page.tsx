990335abe7ca09a429bf2dd68c024ca4977b4d80
Sarim Abbas <sarim.stays@gmail.com>
2023-04-28 00:09:41 -0700

    add index page

--- packages/protest-web/app/page.tsx
+++ packages/protest-web/app/page.tsx
@@ -1,9 +1,25 @@
-export default function Home() {
+import { getXataClient } from "@/xata";
+import Link from "next/link";
+
+export default async function Home() {
+  const xata = getXataClient();
+  const lists = await xata.db.lists.getMany();
+
   return (
     <main>
       <h1>Lists</h1>
       <br />
-      <div className="p-4 rounded-md bg-amber-200">Sarim's Twitter List</div>
+      {lists.map((l) => {
+        return (
+          <Link
+            className="p-4 rounded-md bg-amber-200"
+            key={l.id}
+            href={`/lists/${l.id}`}
+          >
+            {l.name}
+          </Link>
+        );
+      })}
     </main>
   );
 }
