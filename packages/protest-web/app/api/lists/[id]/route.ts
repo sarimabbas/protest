0e90839692ccb155f05a79faeb08c70bb825347b
Sarim Abbas <sarim.stays@gmail.com>
2023-04-30 23:50:34 -0700

    wip

---
+++ packages/protest-web/app/api/lists/[id]/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const ZInputItem = z.object({
+  content: z.string(),
+});
+
+const ZPostBody = z.object({
+  inputs: z.array(ZInputItem),
+});
+
+const ZOutputItem = z.object({
+  show: z.boolean(),
+});
+
+const ZPostResponse = z.object({
+  outputs: z.array(ZOutputItem),
+});
+
+export const POST = async (request: NextRequest) => {
+  const body = await request.json();
+  const parsedBody = ZPostBody.safeParse(body);
+  if (parsedBody.success === false) {
+    return NextResponse.json(parsedBody.error, { status: 400 });
+  }
+
+  const outputs = parsedBody.data.inputs.map((input) => ({
+    show: input.content.length > 0,
+  }));
+
+  const parsedResponse = ZPostResponse.safeParse({ outputs });
+  if (parsedResponse.success === false) {
+    return NextResponse.json(parsedResponse.error, { status: 500 });
+  }
+
+  return NextResponse.json(parsedResponse);
+};
