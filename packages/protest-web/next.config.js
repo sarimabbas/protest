56124f0588350a244ad614bd07ef7bdeb53f2e72
Sarim Abbas <sarim.stays@gmail.com>
2023-05-09 01:03:56 -0700

    wip

--- packages/protest-web/next.config.js
+++ packages/protest-web/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  transpilePackages: ["@protest/tailwind", "@protest/shared"],
+  transpilePackages: ["@protest/tailwind", "@protest/shared", "funky"],
 };
 
 module.exports = nextConfig;
